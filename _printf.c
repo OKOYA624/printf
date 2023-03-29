285309cec6ca61d2f7f22fe0ac76cf52f3a8350a
OKOYA624 <54579463+OKOYA624@users.noreply.github.com>
2023-03-29 20:20:54 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
-* _printf - Print formatted output to stdout
-* @format: A string that specifies the format of the output
-* Return: The number of characters printed (excluding the null byte used
-*/
+ * _printf - Print formatted output to stdout
+ *
+ * @format: A string that specifies the format of the output
+ *
+ * Return: The number of characters printed (excluding the null byte used
+ *         to end output to strings), or -1 if an error occurs
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -16,7 +20,6 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-<<<<<<< HEAD
 			format++;
 			switch (*format)
 			{
@@ -26,7 +29,7 @@ int _printf(const char *format, ...)
 				case 's':
 					count += printf("%s", va_arg(args, char *));
 					break;
-case 'd':
+					case 'd':
 				case 'i':
 					count += printf("%d", va_arg(args, int));
 					break;
@@ -41,31 +44,12 @@ case 'd':
 		else
 		{
 			count += printf("%c", *format);
-=======
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1); */
-			print_char++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list,
-				buffer, flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
->>>>>>> 28414cd2ac9b96b61dc461fdc2cf806c3876adbc
 		}
 
 		format++;
 	}
+
 	va_end(args);
+
 	return (count);
 }
